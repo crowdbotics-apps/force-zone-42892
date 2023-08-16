cc458e225115370d0be1a38cdfedb92e3e086f03
Crowdbotics <team@crowdbotics.com>
2023-08-16 23:19:02 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "force-zone-42892",
+  url: "https://force-zone-42892.botics.co",
+  api: "https://force-zone-42892.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
